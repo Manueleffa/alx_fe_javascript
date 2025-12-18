993410f1977c9d5a7a90cb4430bb61762730d5d3
Emmanuel Effa <manueleffa@gmail.com>
2025-12-18 16:14:34 +0100

    Created a Dynamic Content Filtering System Using Web Storage and JSON

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -25,8 +25,9 @@ const firstInput = document.createElement("input");
 const secondInput = document.createElement("input");
 const formBtn = document.createElement("button");
 const itemFromLocalStorage = localStorage.getItem("Quotes");
-const exportBtn = document.querySelector('#exportBtn');
-const importBtn = document.querySelector('#importFile');
+const exportBtn = document.querySelector("#exportBtn");
+const importBtn = document.querySelector("#importFile");
+const categoryDropdown = document.getElementById("categoryFilter");
 
 function showRandomQuote() {
   const randomIndex = Math.floor(Math.random() * quoteArr.length);
@@ -75,6 +76,7 @@ function addQuote(e) {
   }
 
   saveQuotes();
+  populateCategories();
 }
 
 function saveQuotes() {
@@ -82,40 +84,84 @@ function saveQuotes() {
 }
 
 function initializeQuotes() {
-  if (itemFromLocalStorage !== null && itemFromLocalStorage !== '') {
+  if (itemFromLocalStorage !== null && itemFromLocalStorage !== "") {
     quoteArr = JSON.parse(itemFromLocalStorage);
   }
 }
 
-
-
 function importFromJsonFile(event) {
   const fileReader = new FileReader();
-  fileReader.onload = function(event) {
+  fileReader.onload = function (event) {
     const importedQuotes = JSON.parse(event.target.result);
     quoteArr.push(...importedQuotes);
     saveQuotes();
-    alert('Quotes imported successfully!');
+    alert("Quotes imported successfully!");
   };
   fileReader.readAsText(event.target.files[0]);
 }
 
-function exportQuotes(){
-    const exporData = JSON.stringify(quoteArr);
+function exportQuotes() {
+  const exporData = JSON.stringify(quoteArr);
 
-    const blob = new Blob([exporData], {type: "application/json"}); 
-    const url = URL.createObjectURL(blob)
+  const blob = new Blob([exporData], { type: "application/json" });
+  const url = URL.createObjectURL(blob);
 
-    const a = document.createElement('a');
-    a.href = url;
-    a.download = 'data.json';
-    a.click();
-    URL.revokeObjectURL(url);
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "data.json";
+  a.click();
+  URL.revokeObjectURL(url);
+}
+
+function populateCategories() {
+  catArr = [];
+
+  while (categoryDropdown.options.length > 1) {
+    categoryDropdown.remove(1);
+  }
+
+  quoteArr.forEach((item) => {
+    catArr.push(item.category);
+  });
+
+  const uniqueCategories = [...new Set(catArr)];
+  uniqueCategories.forEach((cat) => {
+    const option = document.createElement("option");
+    option.value = cat;
+    option.textContent = cat;
+
+    categoryDropdown.appendChild(option);
+  });
+
+  const savedCategory = localStorage.getItem("savedCategory");
+  if (savedCategory) {
+    categoryDropdown.value = savedCategory;
+  }
+}
+
+function filterQuotes() {
+  const selectedCategory = categoryDropdown.value.toLowerCase();
+  if (selectedCategory !== "all") {
+    localStorage.setItem("savedCategory", selectedCategory);
+
+    const setCat = quoteArr.filter((item) => {
+      return item.category.toLowerCase() === selectedCategory;
+    });
+
+    const randomIndex = Math.floor(Math.random() * setCat.length);
+    const quote = setCat[randomIndex];
+
+    quoteDisplay.innerHTML = `<p><q>${quote.text}</q> <br> ${quote.category}</p>`;
+  } else {
+    showRandomQuote();
+  }
 }
 
 function domContentLoad() {
-    initializeQuotes()
-    createAddQuoteForm();
+  initializeQuotes();
+  createAddQuoteForm();
+  populateCategories();
+  filterQuotes();
 }
 
 // Event Litsnener
@@ -123,4 +169,5 @@ newQuoteBtn.addEventListener("click", showRandomQuote);
 document.addEventListener("DOMContentLoaded", domContentLoad);
 formBtn.addEventListener("click", addQuote);
 exportBtn.addEventListener("click", exportQuotes);
-importBtn.addEventListener("onchange", importFromJsonFile)
+importBtn.addEventListener("change", importFromJsonFile);
+categoryDropdown.addEventListener("change", filterQuotes);
